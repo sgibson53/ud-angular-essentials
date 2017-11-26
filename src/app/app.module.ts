58d86266cf9ff53e4b08f6910f059fd6395a2b7b
Seth Gibson <wgibson53@gmail.com>
2017-11-26 10:36:40 -0600

    Reset base

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { UserDetailComponent } from './user-detail/user-detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    UserDetailComponent
+    AppComponent
   ],
   imports: [
     BrowserModule
